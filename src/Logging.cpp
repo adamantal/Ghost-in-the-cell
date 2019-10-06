34d9a1b09a81d176475dcba0666faf5b4ecafadb
antala <aadam99@gmail.com>
2019-10-06 15:44:01 +0200

    Moving log dir creation from compile time to runtime.

--- src/Logging.cpp
+++ src/Logging.cpp
@@ -1,5 +1,7 @@
 #include "Logging.hpp"
 
+#include <experimental/filesystem>
+
 void setupLogger(bool debug) {
     spdlog::set_level(spdlog::level::debug);
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
@@ -8,6 +10,9 @@ void setupLogger(bool debug) {
     } else {
         console_sink->set_level(spdlog::level::info);
     }
+
+    std::experimental::filesystem::create_directory("./logs");
+    std::experimental::filesystem::create_directory("./logs/runs");
 
     auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/main.txt", true);
     file_sink->set_level(spdlog::level::debug);
